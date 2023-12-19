bd2e4618f70d5bf6f07855bef9442aa09ec384fa
dc-choi <cheek0805@naver.com>
2023-12-19 09:32:06 +0900

    Revert "자바 문법 람다식 공부"
    This reverts commit ff8577e9e159b930ac579dee05ab58784ce04351.

--- src/test/java/com/example/javadatastructure/grammar/lambda/MyFunctionalInterface.java
+++
@@ -1,7 +0,0 @@
-package com.example.javadatastructure.grammar.lambda;
-
-@FunctionalInterface
-public interface MyFunctionalInterface {
-    public void method();
-    // public void method(); // 컴파일 오류
-}
--- src/test/java/com/example/javadatastructure/grammar/lambda/MyFunctionalInterface2.java
+++
@@ -1,7 +0,0 @@
-package com.example.javadatastructure.grammar.lambda;
-
-@FunctionalInterface
-public interface MyFunctionalInterface2 {
-    public void method(int x);
-    // public void method(); // 컴파일 오류
-}
