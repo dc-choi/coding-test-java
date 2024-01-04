5fb2bed545cfef8c0ff58fd247115bbe80dd4500
dc-choi <cheek0805@naver.com>
2024-01-04 17:49:14 +0900

    코딩테스트 문제풀이
    코딩테스트 문제풀이를 진행하였습니다.

--- src/test/java/com/example/javadatastructure/codingtest/CodingTestIntro.java
+++ src/test/java/com/example/javadatastructure/codingtest/CodingTestIntro.java
@@ -1073,4 +1073,38 @@ public class CodingTestIntro {
 
         System.out.println(answer);
     }
+
+    @Test
+    void 문자열의_앞의_n글자() {
+        String my_string = "ProgrammerS123";
+        int n = 11;
+
+        System.out.println(my_string.substring(0, n));
+    }
+
+    @Test
+    void 조건에_맞게_수열_반환하기_3() {
+        int[] arr = { 1, 2, 3, 100, 99, 98 };
+        int k = 3;
+
+        int[] answer = Arrays.stream(arr).map(a -> k % 2 == 0 ? a + k : a * k).toArray();
+
+        for (int a : answer) {
+            System.out.println(a);
+        }
+    }
+
+    @Test
+    void n의_배수() {
+        int num = 34;
+        int n = 3;
+
+        int answer = 0;
+
+        if (num % n == 0) {
+            answer = 1;
+        }
+
+        System.out.println(answer);
+    }
 }
